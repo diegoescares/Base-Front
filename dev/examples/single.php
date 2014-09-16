4bd629ffa75994bacf8b09aa35c496e8fe09024e
diegoescares <diegoescares@gmail.com>
2014-09-16 14:48:18 -0300

    Colores y contenido

---
+++ dev/examples/single.php
@@ -0,0 +1,43 @@
+<?php require("header.php"); ?>
+
+<div class="limit">
+	<div class="inner">
+
+		<div class="content">
+
+			<h1>Descubre: Lorem ipsum dolor sit amet</h1>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum aliquam, ipsa nobis atque dicta possimus voluptatem libero tenetur facere? Enim nostrum ducimus labore ullam corporis velit nesciunt id, doloribus eligendi!</p>
+
+			<p><img src="http://placehold.it/700x400/3e444b" alt="Image"></p>
+
+			<p>Ratione, <strong>obcaecati quia eligendi</strong> voluptatem perferendis amet suscipit ullam enim voluptatibus voluptas praesentium pariatur qui porro libero ipsum consectetur sapiente culpa aperiam omnis animi iusto magnam laboriosam. Ea consequuntur, alias.</p>
+			<p>Excepturi inventore maiores, non repellendus quidem <a href="#">necessitatibus soluta</a> doloribus, dolorum velit explicabo a nobis, fugit natus molestiae placeat minus suscipit itaque commodi ex sit, aspernatur laudantium deserunt porro. Voluptate, ea.</p>
+
+			<h2>Lorem ipsum dolor sit amet</h2>
+			
+			<blockquote>
+			<p>Cum corporis doloribus, laudantium molestias. Hic officiis repellat illum temporibus adipisci harum, eaque provident amet perspiciatis! Facere repudiandae non facilis officia, nihil.</p>
+			</blockquote>
+
+			<p>Error nostrum repellendus inventore molestiae nam aspernatur cumque, cum, eaque mollitia. Quod aliquid tempore, exercitationem facilis laborum ratione quas facere iste, blanditiis earum possimus eligendi voluptatem aperiam nobis, incidunt officiis?</p>
+
+			<p>Vitae ex libero repellat iure tenetur facere, laudantium pariatur. Totam ipsa error sit laboriosam deserunt eos, voluptate ea, praesentiu.</p>
+
+			<h3>Lorem ipsum dolor sit amet</h3>
+
+			<p>Minus vel ad ipsa pariatur itaque. Ducimus quos accusantium aperiam assumenda, obcaecati dignissimos distinctio in rem. Autem repellat, quo aliquam fuga, repudiandae tenetur pariatur, vero ratione iusto, labore nam ipsam.</p>
+			<div class="important">
+				<p>Totam quasi sit laborum nesciunt consectetur, ex accusamus deleniti voluptatem architecto consequuntur accusantium odit ratione quia minima error recusandae in qui repellendus porro ullam non. Distinctio eaque tempore architecto ab!</p>
+			</div>
+
+			<h4>Lorem</h4>
+			<p>Vero sequi, commodi tempore doloribus aperiam culpa obcaecati, eveniet quibusdam, excepturi quia aut voluptatem dignissimos aspernatur quaerat sunt corrupti eligendi, distinctio ex maxime at odit dolore. Ad debitis deserunt cumque.</p>
+			
+		</div>
+
+	</div>
+</div>
+
+
+<?php require("footer.php"); ?>
