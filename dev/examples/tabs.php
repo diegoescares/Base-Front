22581bb8158b62af0543f71b0b9dc064c4adaf56
diegoescares <diegoescares@gmail.com>
2014-11-17 11:07:30 -0300

    Tabs example

---
+++ dev/examples/tabs.php
@@ -0,0 +1,28 @@
+<?php require("header.php"); ?>
+
+
+<div class="inner">
+
+	<div class="tabs">
+		<div class="tabs-header">
+			<a href="#" class="tab">dsad</a>
+			<a href="#" class="tab">dsad</a>
+		</div>
+		<div class="tabs-body">
+			<div class="tab">
+				<div class="inner">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto libero, commodi, at ab facere saepe. Sapiente ullam architecto ea voluptatibus harum. Tempore repellendus veritatis voluptates vel culpa voluptatem facilis, dolorum!	
+				</div>
+			</div>
+			<div class="tab">
+				<div class="inner">
+					Tempora, expedita hic dolorem id, nisi voluptas tempore non laudantium mollitia? Blanditiis eum ab soluta veniam ipsa accusamus quasi, possimus hic aut sapiente neque voluptas nostrum accusantium quae distinctio id.
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+    
+
+<?php require("footer.php"); ?>
